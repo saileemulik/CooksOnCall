882025bf48a6574479719e9dc31391387b2e8b52
Sailee Mulik <130562994+saileemulik@users.noreply.github.com>
2024-06-10 16:36:39 +0530

    Delete backend directory

--- backend/router/reg-router.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const regForm = require("../controllers/reg-controller");
-router.route("/reg").post(regForm);
-
- module.exports = router;
